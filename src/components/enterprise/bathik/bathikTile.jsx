48e9f5c24fd79399eccea3a4fad48f2d2d703a62
Yasantha Fonseka <Y.Fonseka@sana-commerce.com>
2022-03-03 23:24:49 +0530

    Bathik Tiles loaded from the database data

---
+++ src/components/enterprise/bathik/bathikTile.jsx
@@ -0,0 +1,25 @@
+import "./bathik.css";
+
+const BathikTile = ({ bathik }) => {
+  return (
+    <>
+      <div class="col-lg-4 mb-3">
+        <div class="card">
+          <img src="/img/bl7.jpg" alt="" class="card-img-top" />
+          <div class="card-body">
+            <h5 class="card-title">Sunset</h5>
+            <p class="card-text">{bathik.description}</p>
+            <a href="" class="btn btn-outline-success btn-lg me-2 ">
+              Read More
+            </a>
+            <a href="" class="btn btn-outline-danger btn-lg me-2 ">
+              <i class="fa fa-heart-o "></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BathikTile;
--- src/database/firebase-config.jsx
+++ src/database/firebase-config.jsx
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import {getFirestore } from "@firebase/firestore";
+import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDORl_IzISIEAP9LnTBQPYr054gHMGxuLc",
-    authDomain: "handsofthambapanni.firebaseapp.com",
-    projectId: "handsofthambapanni",
-    storageBucket: "handsofthambapanni.appspot.com",
-    messagingSenderId: "288518372281",
-    appId: "1:288518372281:web:06e7bbc520569d4b479468",
-    measurementId: "G-WBYJ8BJ2VF"
+  apiKey: "AIzaSyDt-P74LG6y5fUjjPDPf_m4EWm5uQ537SU",
+  authDomain: "hot-rot.firebaseapp.com",
+  projectId: "hot-rot",
+  storageBucket: "hot-rot.appspot.com",
+  messagingSenderId: "44108530183",
+  appId: "1:44108530183:web:24f75cb56b7d85ebb460a2",
+  measurementId: "G-V7VPCE8YV9",
 };
 
 // Initialize Firebase
@@ -17,8 +17,4 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
-
 export default app;
-
-
-
