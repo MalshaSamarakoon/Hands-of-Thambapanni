4fa169b4e462a498958fffb70bd27351f2216e4b
sanduni <cst18045@std.uwu.ac.lk>
2022-03-08 15:05:40 +0530

    feedbackreport

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
+//Calling Bootstrap 4.5 css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Calling Firebase config setting to call the data
+import { db } from './firebaseconfig';
+import { useState, useEffect } from 'react';
+import { collection, doc, getDocs } from 'firebase/firestore';
+import * as ReactDOM from "react-dom";
+import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
+
 function App() {
+
+ const [feedback, SetFeedback] = useState([]);
+  const feedbackCollectionref = collection(db, "contacts")
+
+  useEffect(() => {
+    const getFeedback = async () => {
+      const data = await getDocs(feedbackCollectionref);
+      
+      SetFeedback(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
+    };
+    
+    getFeedback();
+  }, []);
+
+    const pdfExportComponent = React.useRef(null);
+
+  const exportPDFWithComponent = () => {
+    if (pdfExportComponent.current) {
+      pdfExportComponent.current.save();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+    <div>
+      <div className="example-config">
+        <button
+          className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base"
+          onClick={exportPDFWithComponent}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          Export to PDF
+        </button>
+        
+      </div>
+  <PDFExport ref={pdfExportComponent} paperSize="A4">
+      <div className="MainDiv">
+        <div class="jumbotron text-center bg-sky">
+          <h3>Feedbacks Report</h3>
+        </div>
+    
+        <div className="container">
+          <table id="example" class="display table">
+            <thead class="thead-dark">
+              <tr>
+                <th>Email</th>
+                <th>Name</th>
+                <th>Massege</th>
+                    
+              </tr>
+            </thead>
+            <tbody> 
+             
+         
+             {feedback.map((feedback) => {
+               return (
+                 <tr>
+                   <td>{feedback.email}</td>
+                   <td>{feedback.name}</td>
+                   <td>{feedback.message}</td>
+                    
+                 </tr>
+                    
+               
+               )
+             })}
+             
+            
+              
+        
+               
+            </tbody>
+            
+          </table>
+          
+        </div>
+     </div>
+     
+    </PDFExport>
+   </div>
+    );
+
+            }
 
 export default App;
---
+++ src/firebaseconfig.jsx
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+import {getsDocs, collection} from "@firebase/firestore"
+
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHdy81J14Ctda_a9f5_SMrs0Gs2dPijgI",
+  authDomain: "project-506d7.firebaseapp.com",
+  databaseURL: "https://project-506d7-default-rtdb.firebaseio.com",
+  projectId: "project-506d7",
+  storageBucket: "project-506d7.appspot.com",
+  messagingSenderId: "524485329305",
+  appId: "1:524485329305:web:6fa99ecb5397afdbbb5308",
+  measurementId: "G-B221G8Y78T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
