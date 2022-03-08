b1bba22dcf45cd55e1b8e2378531755f7723d49c
Yasantha Fonseka <Y.Fonseka@sana-commerce.com>
2022-03-08 23:48:12 +0530

    Integrating contact and rating

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,160 +1,178 @@
-import {useState} from 'react'
-import emailjs from 'emailjs-com'
-import {Header} from "./header";
-
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import { Header } from "./header";
+import { db } from "../database/firebase-config";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import Rate from "./rating";
+import { useAlert } from "react-alert";
 
 const initialState = {
-    name: '',
-    email: '',
-    message: '',
-}
-export const Contact = (props) => {
-    const [{name, email, message}, setState] = useState(initialState)
+  name: "",
+  email: "",
+  message: "",
+};
+const Contact = (props) => {
+  const alert = useAlert();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-    const handleChange = (e) => {
-        const {name, value} = e.target
-        setState((prevState) => ({...prevState, [name]: value}))
-    }
-    const clearState = () => setState({...initialState})
+  const [loader, setLoader] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoader(true);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(name, email, message)
-        emailjs
-            .sendForm(
-                'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
-            )
-            .then(
-                (result) => {
-                    console.log(result.text)
-                    clearState()
-                },
-                (error) => {
-                    console.log(error.text)
-                }
-            )
-    }
-    return (
-        <div className="bg-blue">
-            <div id='contact'>
-                <div className='container'>
-                    <div className='col-md-8'>
-                        <div className='row'>
-                            <div className='section-title'>
-                                <h2>Get In Touch</h2>
-                                <p>
-                                Send an email to get in touch with us. We will get back to you at the soonest possible!
-                                </p>
-                            </div>
-                            <form name='sentMessage' validate onSubmit={handleSubmit}>
-                                <div className='row'>
-                                    <div className='col-md-6'>
-                                        <div className='form-group'>
-                                            <input
-                                                type='text'
-                                                id='name'
-                                                name='name'
-                                                className='form-control'
-                                                placeholder='Name'
-                                                required
-                                                onChange={handleChange}
-                                            />
-                                            <p className='help-block text-danger'></p>
-                                        </div>
-                                    </div>
-                                    <div className='col-md-6'>
-                                        <div className='form-group'>
-                                            <input
-                                                type='email'
-                                                id='email'
-                                                name='email'
-                                                className='form-control'
-                                                placeholder='Email'
-                                                required
-                                                onChange={handleChange}
-                                            />
-                                            <p className='help-block text-danger'></p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className='form-group'>
+    await addDoc(collection(db, "contacts"), {
+      name,
+      email,
+      message,
+    })
+      .then(() => {
+        setLoader(false);
+        alert.show("Your message has been submitted👍");
+      })
+      .catch((error) => {
+        alert(error.message);
+        setLoader(false);
+      });
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <div className="bg-blue">
+      <div id="contact">
+        <div className="container">
+          <div className="col-md-8">
+            <div className="row">
+              <div className="section-title">
+                <h2>Get In Touch</h2>
+                <p>
+                  Send an email to get in touch with us. We will get back to you
+                  at the soonest possible!
+                </p>
+              </div>
+              <form name="sentMessage" validate onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="form-control"
+                        placeholder="Name"
+                        required
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="Email"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group">
                   <textarea
-                      name='message'
-                      id='message'
-                      className='form-control'
-                      rows='4'
-                      placeholder='Message'
-                      required
-                      onChange={handleChange}
+                    name="message"
+                    id="message"
+                    className="form-control"
+                    rows="4"
+                    placeholder="Message"
+                    required
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                   ></textarea>
-                                    <p className='help-block text-danger'></p>
-                                </div>
-                                <div id='success'></div>
-                                <button type='submit' className='btn btn-custom btn-lg'>
-                                    Send Message
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                    <div className='col-md-3 col-md-offset-1 contact-info'>
-                        <div className='contact-item'>
-                            <h3>Contact Info</h3>
-                            <p>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div id="success"></div>
+                <button type="submit" className="btn btn-custom btn-lg">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="col-md-3 col-md-offset-1 contact-info">
+            <div className="contact-item">
+              <h3>Contact Info</h3>
+              <p>
                 <span>
-                  <i className='fa fa-map-marker'></i> Address
+                  <i className="fa fa-map-marker"></i> Address
                 </span>
-                                Uva Wellasa University, Passara Road, Badulla, Sri Lanka
-                            </p>
-                        </div>
-                        <div className='contact-item'>
-                            <p>
-                <span>
-                  <i className='fa fa-phone'></i> Phone
-                </span>{' '}
-                                +94 712223619
-                            </p>
-                        </div>
-                        <div className='contact-item'>
-                            <p>
-                <span>
-                  <i className='fa fa-envelope-o'></i> Email
-                </span>{' '}
-                                racuwub@gmail.com
-                            </p>
-                        </div>
-                    </div>
-                    <div className='col-md-12'>
-                        <div className='row'>
-                            <div className='social'>
-                                <ul>
-                                    <li>
-                                        <a href='https://www.facebook.com/RACUWUBadulla/'>
-                                            <i className='fa fa-facebook'></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href='https://www.linkedin.com/in/rotaract-club-of-uva-wellassa-university-449b33192/?originalSubdomain=lk'>
-                                            <i className='fa fa-linkedin'></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href={props.data ? props.data.youtube : '/'}>
-                                            <i className='fa fa-youtube'></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                Uva Wellasa University, Passara Road, Badulla, Sri Lanka
+              </p>
             </div>
-            <div id='footer'>
-                <div className='container text-center'>
-                    <p>
-                        &copy; A Project By The Rotaract Club Of Uva Wellassa University - Badulla
-                    </p>
-                </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-phone"></i> Phone
+                </span>{" "}
+                +94 712223619
+              </p>
             </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-envelope-o"></i> Email
+                </span>{" "}
+                racuwub@gmail.com
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>Rate Us :</p>
+              <Rate />
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="row">
+              <div className="social">
+                <ul>
+                  <li>
+                    <a href="https://www.facebook.com/RACUWUBadulla/">
+                      <i className="fa fa-facebook"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/rotaract-club-of-uva-wellassa-university-449b33192/?originalSubdomain=lk">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.youtube : "/"}>
+                      <i className="fa fa-youtube"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div id="footer">
+        <div className="container text-center">
+          <p>
+            &copy; A Project By The Rotaract Club Of Uva Wellassa University -
+            Badulla
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Profile from "./profiles";
 import About from "./about";
 import Features from "./features";
-import { Contact } from "./contact";
+import Contact from "./contact";
 import { Header } from "./header";
 import { Navigation } from "./navigation";
 import HomeCarousel from "../HomeCarousel";
